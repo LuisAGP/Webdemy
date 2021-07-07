2f8ee0bc71dc3aac498f0279c6d9724c6bfbc244
LuisGP <luis.lagp4598@gmail.com>
2021-07-07 00:39:36 +0000

    First commit

---
+++ webdemy/webdemy/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for webdemy project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webdemy.settings')
+
+application = get_wsgi_application()
